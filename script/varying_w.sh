709a78e994f7ca47d627f612fcb63b306fe38d7e
Giulio Ermanno Pibiri <jeis90@gmail.com>
2024-05-25 10:48:00 +0200

    added all methods to script

--- script/varying_w.sh
+++ script/varying_w.sh
@@ -49,11 +49,35 @@ done
 # k = 31
 for w in {2..8}
 do
-    ./density -i random.10M.fa -k 31 -w $w -a rot-minimizer --stream 2>> rot-minimizer.varying_w.k.31.txt
+    ./density -i random.10M.fa -k 31 -w $w -a rot-minimizer-alt --stream 2>> rot-minimizer-alt.varying_w.k.31.txt
 done
 
 # k = 127
 for w in {2..24}
 do
-    ./density -i random.10M.fa -k 127 -w $w -a rot-minimizer --stream 2>> rot-minimizer.varying_w.k.127.txt
+    ./density -i random.10M.fa -k 127 -w $w -a rot-minimizer-alt --stream 2>> rot-minimizer-alt.varying_w.k.127.txt
+done
+
+# k = 31
+for w in {2..8}
+do
+    ./density -i random.10M.fa -k 31 -w $w -a rot-minimizer-orig --stream 2>> rot-minimizer-orig.varying_w.k.31.txt
+done
+
+# k = 127
+for w in {2..24}
+do
+    ./density -i random.10M.fa -k 127 -w $w -a rot-minimizer-orig --stream 2>> rot-minimizer-orig.varying_w.k.127.txt
+done
+
+# k = 31
+for w in {2..8}
+do
+    ./density -i random.10M.fa -k 31 -w $w -a decycling --stream 2>> decycling.varying_w.k.31.txt
+done
+
+# k = 127
+for w in {2..24}
+do
+    ./density -i random.10M.fa -k 127 -w $w -a double-decycling --stream 2>> double-decycling.varying_w.k.127.txt
 done
